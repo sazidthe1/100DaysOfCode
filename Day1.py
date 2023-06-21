27b457b96c9f40352caefb7ec7340292de68aa3d
Sazidul Islam <sazidthe1@gmail.com>
2023-06-21 22:53:54 +0600

    Added more on 1st code in Day1.py

--- Day1.py
+++ Day1.py
@@ -1,6 +1,54 @@
+# Creating code with Python
 # Printing 1st code
-print("Hello World!")
+print("Hello, World!")
 
-# Defining a variable and printing "Hello World!"" using the variable
-greetings = "Hello World!"
-print(greetings)
+# Improving the 1st Python program
+input("What's your name? ")
+print("hello, World!")
+
+# Defining a variable name and printing "hello" with the variable
+name = input("What's your name? ")
+print("Hello,")
+print(name)
+
+
+# Comments
+# Ask the user for their name
+name = input("What's your name? ")
+print("hello,")
+print(name)
+
+
+# Pseudocode
+# Ask the user for their name
+name = input("What's your name? ")
+
+# Print Hello
+print("Hello,")
+
+# Print the name inputted
+print(name)
+
+
+# Further improving the 1st Python program
+# Ask the user for their name
+name = input("What's your name? ")
+
+# Print hello and the inputted name
+print("Hello,", name)
+
+# Strings and Paremeters
+# Ask the user for their name
+name = input("What's your name? ")
+print("hello,")
+print(name)
+
+# Ask the user for their name
+name = input("What's your name? ")
+print("Hello,", end="")
+print(name)
+
+# Formatting Strings
+# Ask the user for their name
+name = input("What's your name? ")
+print(f"Hello, {name}!")
